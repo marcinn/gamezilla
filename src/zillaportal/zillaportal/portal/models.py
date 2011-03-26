049b9f59440755edb94f694629daba764dde333a
w4rri0r3k <w4rri0r3k@gmail.com>
2011-03-26 18:07:04 +0000

    Added methods to models.py Some updates

--- src/zillaportal/zillaportal/portal/models.py
+++ src/zillaportal/zillaportal/portal/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.core.exceptions import ObjectDoesNotExist
+import datetime
 
 
 class Profile(models.Model):
@@ -8,24 +11,78 @@ class Profile(models.Model):
     avatar_image = models.ImageField(upload_to='images', null=True, blank=True)
     level =  models.IntegerField(default=0)
     about = models.CharField(max_length=500, default='')
+    
+    def __unicode__(self):
+        return self.user.username
 
 class Game(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    screenshot = models.ImageField(upload_to='images')
+    screenshot = models.ImageField(upload_to='images', null=True, blank=True)
     games_count = models.IntegerField()
     #client = wtf?
     #gameserver = wtf?
+    
+    def __unicode__(self):
+        return self.title
+
+    def get_active_gameplays(self):
+        return self.gameplays.filter(status='S')
+
+    
+
+STATUS_CHOICES = (
+('S', 'Started'),
+('W', 'Waiting for players'),
+('F', 'Game finished'),
+)
 
 class Gameplay(models.Model):
     created_at = models.DateTimeField()
     started_at = models.DateTimeField()
     end_at =  models.DateTimeField()
-    status = models.CharField(max_length=1)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     owner = models.ForeignKey(User,  related_name='owned_games')
     player = models.ManyToManyField(User, related_name='playing_game')
-    game = models.ForeignKey(Game)
+    game = models.ForeignKey(Game, related_name='gameplays')
+
+    def __unicode__(self):
+        name = self.game.title
+        name += " Owner: "+self.owner.username
+        name += " Players: "
+        for p in self.player.all():
+            name+= p.username+" "
+        return name
+
+    def start(self):
+        self.status = "S"
+        self.started_at = datetime.datetime.now()
+        self.save()
+
+    def finish(self):
+        self.status = "F"
+        self.end_at = datetime.datetime.now()
+        self.save()
+
+
 
 
 class Ranking(models.Model):
     score = models.IntegerField()
+
+
+#######################
+# Signals
+######################
+def create_profile(sender, **kwargs):
+    # Your specific logic here
+    try:
+        kwargs['instance'].get_profile()
+    except ObjectDoesNotExist:
+	profile = Profile ()
+        profile.user = kwargs['instance']
+        profile.save()
+
+
+post_save.connect(create_profile, sender=User)
+#########################
